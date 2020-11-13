1acb7d4f68b4506afe00eed2538d2b7fabc5fbd5
Yi-Chen <yichen@powcyc.com>
2020-11-13 16:05:12 +0800

    add 2 AE model

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from keras.layers import (Activation, BatchNormalization,
-                          Conv2D, Dense, Input, Flatten, MaxPooling2D, UpSampling2D)
+                          Conv2D, Dense, Input, Flatten, MaxPooling2D, UpSampling2D, regularizers)
 from keras.models import Model
 
 
@@ -33,7 +33,7 @@ def NN_model_1():
     return model
 
 
-def AEN_model_1():
+def AE_model_1():
     # Defining our Image denoising autoencoder
     Input_img = Input(shape=(960, 1280, 3))
 
@@ -53,6 +53,127 @@ def AEN_model_1():
     x1 = Conv2D(16, (3, 3), activation='relu', padding='same')(x2)
     x1 = UpSampling2D((2, 2))(x1)
     decoded = Conv2D(1, (3, 3), activation='tanh', padding='same')(x1)
+
+    autoencoder = Model(Input_img, decoded)
+    return autoencoder
+
+
+def AE_model_2():
+    Input_img = Input(shape=(960, 1280, 3))
+
+    # Encoding Architecture
+    # Block 1
+    x1 = Conv2D(16, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block1_conv1')(Input_img)
+    x1 = BatchNormalization(name='block1_bn1')(x1)
+    x1 = Activation('relu', name='block1_ac1')(x1)
+    x1 = Conv2D(16, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block1_conv2')(x1)
+    x1 = BatchNormalization(name='block1_bn2')(x1)
+    x1 = Activation('relu', name='block1_ac2')(x1)
+    x1 = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='block1_pool')(x1)
+
+    # Block 2
+    x2 = Conv2D(32, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block2_conv1')(x1)
+    x2 = BatchNormalization(name='block2_bn1')(x2)
+    x2 = Activation('relu', name='block2_ac1')(x2)
+    x2 = Conv2D(32, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block2_conv2')(x2)
+    x2 = BatchNormalization(name='block2_bn2')(x2)
+    x2 = Activation('relu', name='block2_ac2')(x2)
+    x2 = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='block2_pool')(x2)
+
+    # Block 3
+    x3 = Conv2D(64, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block3_conv1')(x2)
+    x3 = BatchNormalization(name='block3_bn1')(x3)
+    x3 = Activation('relu', name='block3_ac1')(x3)
+    x3 = Conv2D(64, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block3_conv2')(x3)
+    x3 = BatchNormalization(name='block3_bn2')(x3)
+    x3 = Activation('relu', name='block3_ac2')(x3)
+    x3 = Conv2D(64, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block3_conv3')(x3)
+    x3 = BatchNormalization(name='block3_bn3')(x3)
+    x3 = Activation('relu', name='block3_ac3')(x3)
+    x3 = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='block3_pool')(x3)
+
+    # Block 4
+    x4 = Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block4_conv1')(x3)
+    x4 = BatchNormalization(name='block4_bn1')(x4)
+    x4 = Activation('relu', name='block4_ac1')(x4)
+    x4 = Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block4_conv2')(x4)
+    x4 = BatchNormalization(name='block4_bn2')(x4)
+    x4 = Activation('relu', name='block4_ac2')(x4)
+    x4 = Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block4_conv3')(x4)
+    x4 = BatchNormalization(name='block4_bn3')(x4)
+    x4 = Activation('relu', name='block4_ac3')(x4)
+    x4 = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='block4_pool')(x4)    
+
+    # Block 5
+    x5 = Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block5_conv1')(x4)
+    x5 = BatchNormalization(name='block5_bn1')(x5)
+    x5 = Activation('relu', name='block5_ac1')(x5)
+    x5 = Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block5_conv2')(x5)
+    x5 = BatchNormalization(name='block5_bn2')(x5)
+    x5 = Activation('relu', name='block5_ac2')(x5)
+    x5 = Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block5_conv3')(x5)
+    x5 = BatchNormalization(name='block5_bn3')(x5)
+    x5 = Activation('relu', name='block5_ac3')(x5)
+    x5 = MaxPooling2D((2, 2), strides=(2, 2), padding='same', name='block5_pool')(x5)       
+
+    encoded = x5
+
+    # Decoding Architecture
+    # Block 6
+    x6 = UpSampling2D((2, 2), name='block6_pool')(encoded)
+    x6 = Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block6_conv1')(x6)
+    x6 = BatchNormalization(name='block6_bn1')(x6)
+    x6 = Activation('relu', name='block6_ac1')(x6)
+    x6 = Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block6_conv2')(x6)
+    x6 = BatchNormalization(name='block6_bn2')(x6)
+    x6 = Activation('relu', name='block6_ac2')(x6)
+    x6 = Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block6_conv3')(x6)
+    x6 = BatchNormalization(name='block6_bn3')(x6)
+    x6 = Activation('relu', name='block6_ac3')(x6)
+
+    # Block 7
+    x7 = UpSampling2D((2, 2), name='block7_pool')(x6)
+    x7 = Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block7_conv1')(x7)
+    x7 = BatchNormalization(name='block7_bn1')(x7)
+    x7 = Activation('relu', name='block7_ac1')(x7)
+    x7 = Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block7_conv2')(x7)
+    x7 = BatchNormalization(name='block7_bn2')(x7)
+    x7 = Activation('relu', name='block7_ac2')(x7)
+    x7 = Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block7_conv3')(x7)
+    x7 = BatchNormalization(name='block7_bn3')(x7)
+    x7 = Activation('relu', name='block7_ac3')(x7)
+
+    # Block 8
+    x8 = UpSampling2D((2, 2), name='block8_pool')(x7)
+    x8 = Conv2D(64, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block8_conv1')(x8)
+    x8 = BatchNormalization(name='block8_bn1')(x8)
+    x8 = Activation('relu', name='block8_ac1')(x8)
+    x8 = Conv2D(64, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block8_conv2')(x8)
+    x8 = BatchNormalization(name='block8_bn2')(x8)
+    x8 = Activation('relu', name='block8_ac2')(x8)
+    x8 = Conv2D(64, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block8_conv3')(x8)
+    x8 = BatchNormalization(name='block8_bn3')(x8)
+    x8 = Activation('relu', name='block8_ac3')(x8)
+
+    # Block 9
+    x9 = UpSampling2D((2, 2), name='block9_pool')(x8)
+    x9 = Conv2D(32, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block9_conv1')(x9)
+    x9 = BatchNormalization(name='block9_bn1')(x9)
+    x9 = Activation('relu', name='block9_ac1')(x9)
+    x9 = Conv2D(32, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block9_conv2')(x9)
+    x9 = BatchNormalization(name='block9_bn2')(x9)
+    x9 = Activation('relu', name='block9_ac2')(x9)
+
+    # Block 10
+    x10 = UpSampling2D((2, 2), name='block10_pool')(x9)
+    x10 = Conv2D(16, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block10_conv1')(x10)
+    x10 = BatchNormalization(name='block10_bn1')(x10)
+    x10 = Activation('relu', name='block10_ac1')(x10)
+    x10 = Conv2D(16, (3, 3), padding='same', kernel_regularizer=regularizers.l2(0.01), kernel_initializer='he_normal', name='block10_conv2')(x10)
+    x10 = BatchNormalization(name='block10_bn2')(x10)
+    x10 = Activation('relu', name='block10_ac2')(x10)
+
+    decoded = Conv2D(1, (3, 3), activation='tanh', padding='same', name='block10_output')(x10)
 
     autoencoder = Model(Input_img, decoded)
     return autoencoder
