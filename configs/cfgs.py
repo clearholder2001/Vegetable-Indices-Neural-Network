0bfc2597604e50014f8491d146062b18c365e51f
Yi-Chen <yichen@powcyc.com>
2020-12-11 18:49:53 +0800

    move datagen_args to cfgs

--- configs/cfgs.py
+++ configs/cfgs.py
@@ -28,3 +28,31 @@ DATA_AUG_BATCH_SIZE = 32
 EPOCHS = 100
 TRAIN_BATCH_SIZE = 32
 VAL_SPLIT = 0.1
+
+
+# ImageDataGenerator
+# ------------------------------------------------
+DATAGEN_ARGS = dict(
+    horizontal_flip=True,
+    vertical_flip=True,
+    rotation_range=20,
+    #width_shift_range=0.1,
+    #height_shift_range=0.1,
+    #brightness_range=(0.5, 1.5),
+    #shear_range=0.3,
+    #zoom_range=0.3,
+    #channel_shift_range=0.1,
+    #rescale=1/255.,
+    #featurewise_center=False,
+    #samplewise_center=False,
+    #featurewise_std_normalization=False,
+    #samplewise_std_normalization=False,
+    #zca_whitening=False,
+    #zca_epsilon=1e-06,
+    fill_mode='nearest',
+    #cval=0.0,
+    #preprocessing_function=None,
+    #data_format=None,
+    validation_split=VAL_SPLIT,
+    #dtype=None,
+)
