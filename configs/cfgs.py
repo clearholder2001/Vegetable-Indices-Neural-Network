05198d571f37b8efa84a21e6b9165e0a20fb6ffa
Yi-Chen <yichen@powcyc.com>
2020-12-16 16:48:33 +0800

    add preprocessing layer to model

--- configs/cfgs.py
+++ configs/cfgs.py
@@ -15,6 +15,7 @@ RESAMPLE_MULTIPLE_FACTOR = 9
 # ------------------------------------------------
 MODEL_NAME = 'AE_v0.2'
 INPUT_LAYER_DIM = (352, 480, 3)
+RANDOMCONTRAST_FACTOR = 0.2
 L2_REGULAR = 0.001
 INIT_LEARNING_RATE = 0.01
 
