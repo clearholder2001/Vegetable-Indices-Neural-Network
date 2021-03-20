aaab9e5a20bb615d5bd6f4cbdff78f407a7426a5
Yi-Chen <yichen@powcyc.com>
2021-03-21 03:15:07 +0800

    move save_result_image

--- utils/image.py
+++ utils/image.py
@@ -34,6 +34,37 @@ def plot_three_images_array(images1, images2, images3, title, idx):
     plt.close(fig)
 
 
+def save_result_image(test_X, test_Y, predict, output_compare=True):
+    assert test_X.shape[0] == test_Y.shape[0] == predict.shape[0], 'Length inconsistent: test_X, test_Y, preditc'
+    path = './fig/inference/'
+    print("Saving result...", end='')
+
+    if output_compare:
+        fig, axs = plt.subplots(1, 3)
+        fig.set_size_inches(12, 4)
+        plt.setp(axs, xticks=[], yticks=[])
+        axs[0].set_title("RGB")
+        img1 = axs[0].imshow(test_X[0], vmin=0, vmax=1)
+        axs[1].set_title("NDVI")
+        img2 = axs[1].imshow(test_Y[0], vmin=-1, vmax=1, cmap=plt.get_cmap('jet'))
+        axs[2].set_title("Predict")
+        img3 = axs[2].imshow(predict[0], vmin=-1, vmax=1, cmap=plt.get_cmap('jet'))
+
+    for i in range(test_X.shape[0]):
+        matplotlib.image.imsave(path + 'rgb/rgb_{0}.jpg'.format(i), test_X[i])
+        matplotlib.image.imsave(path + 'ndvi/ndvi_{0}.jpg'.format(i), np.squeeze(test_Y[i]), cmap=plt.get_cmap('jet'))
+        matplotlib.image.imsave(path + 'predict/predict_{0}.jpg'.format(i), np.squeeze(predict[i]), cmap=plt.get_cmap('jet'))
+        if output_compare:
+            img1.set_data(test_X[i])
+            img2.set_data(test_Y[i])
+            img3.set_data(predict[i])
+            fig.suptitle("Compare #" + str(i), fontsize=24)
+            fig.tight_layout()
+            fig.savefig(path + 'compare/compare_{0}.jpg'.format(i))
+    plt.close()
+    print("Done")
+
+
 def load_data():
     rgb_path = os.path.join('..', 'Jim', 'dataset', '20meter', 'train_20meter_RGB.npy')
     # ndvi_path = os.path.join('..', 'Jim', 'dataset','20meter', 'train_20meter_NDVI.npy')
