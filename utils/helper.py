87e557bd8061e9c86b0e573e1c334bd18f15a942
Yi-Chen <yichen@powcyc.com>
2021-04-20 23:12:03 +0800

    apply preprocessing function for predict

--- utils/helper.py
+++ utils/helper.py
@@ -51,17 +51,3 @@ def calculate_statistics(test_Y, predict):
     print("Final RMSE: {0:.4f}".format(rmse))
     print("Final R Square: {0:.4f}".format(r2))
     print("Final Correlation: {0:.4f}".format(correlation[0]))
-
-
-def create_tf_dataset(array, generator, batch_size):
-    dataset = tf.data.Dataset.from_generator(lambda: generator(array), output_signature=(tf.TensorSpec(shape=array.shape[1:], dtype=tf.float32)))
-    dataset = dataset.batch(batch_size).prefetch(buffer_size=autotune)
-    return dataset
-
-
-def simple_image_generator(images):
-    i = 0
-    while i < images.shape[0]:
-        image = images[i, :, :, :]
-        yield image
-        i += 1
