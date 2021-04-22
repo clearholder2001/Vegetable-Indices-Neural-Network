12db47c3f8972a42aec4461a2b301dc839bfdde8
Yi-Chen <yichen@powcyc.com>
2021-04-22 14:28:04 +0800

    update preprocessing setup

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -30,30 +30,27 @@ def train_preprocessing(train_X, train_Y, batch_size, enable_data_aug, use_image
             validation_image_datagen = ImageDataGenerator(validation_split=val_split)
             validation_mask_datagen = ImageDataGenerator(validation_split=val_split)
 
-            train_mask_datagen.brightness_range = None
-            train_mask_datagen.channel_shift_range = 0.0
+            if train_image_datagen.brightness_range != None:
+                train_mask_datagen.brightness_range = None
+                train_image_datagen.rescale = 1./255
+            if train_image_datagen.channel_shift_range != 0.0:
+                train_mask_datagen.channel_shift_range = 0.0
 
             # train_image_path, train_mask_path, validation_image_path, validation_mask_path = output_init()
-            train_image_generator = train_image_datagen.flow(train_X, batch_size=1, shuffle=True, seed=seed, subset='training')
-            train_mask_generator = train_mask_datagen.flow(train_Y, batch_size=1, shuffle=True, seed=seed, subset='training')
-            validation_image_generator = validation_image_datagen.flow(train_X, batch_size=1, shuffle=True, seed=seed, subset='validation')
-            validation_mask_generator = validation_mask_datagen.flow(train_Y, batch_size=1, shuffle=True, seed=seed, subset='validation')
+            train_image_generator = train_image_datagen.flow(train_X, batch_size=batch_size, shuffle=True, seed=seed, subset='training')
+            train_mask_generator = train_mask_datagen.flow(train_Y, batch_size=batch_size, shuffle=True, seed=seed, subset='training')
+            validation_image_generator = validation_image_datagen.flow(train_X, batch_size=batch_size, shuffle=True, seed=seed, subset='validation')
+            validation_mask_generator = validation_mask_datagen.flow(train_Y, batch_size=batch_size, shuffle=True, seed=seed, subset='validation')
 
             train_generator = zip(train_image_generator, train_mask_generator)
             validation_generator = zip(validation_image_generator, validation_mask_generator)
 
-            def keras_image_generator(generator):
-                for data in generator:
-                    data[0].shape = data[0].shape[1:]
-                    data[1].shape = data[1].shape[1:]
-                    yield data
+            output_signature = (tf.TensorSpec(shape=[None] + list(train_X.shape[1:]), dtype=tf.float32), tf.TensorSpec(shape=[None] + list(train_Y.shape[1:]), dtype=tf.float32))
+            train_ds = tf.data.Dataset.from_generator(lambda: train_generator, output_signature=output_signature)
+            validation_ds = tf.data.Dataset.from_generator(lambda: validation_generator, output_signature=output_signature)
 
-            output_signature = (tf.TensorSpec(shape=train_X.shape[1:], dtype=tf.float32), tf.TensorSpec(shape=train_Y.shape[1:], dtype=tf.float32))
-            train_ds = tf.data.Dataset.from_generator(lambda: keras_image_generator(train_generator), output_signature=output_signature)
-            validation_ds = tf.data.Dataset.from_generator(lambda: keras_image_generator(validation_generator), output_signature=output_signature)
-
-            train_ds = train_ds.batch(batch_size).prefetch(tf.data.AUTOTUNE)
-            validation_ds = validation_ds.batch(batch_size).prefetch(tf.data.AUTOTUNE)
+            train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
+            validation_ds = validation_ds.prefetch(tf.data.AUTOTUNE)
 
             return train_ds, validation_ds
         else:
@@ -74,19 +71,19 @@ def train_preprocessing(train_X, train_Y, batch_size, enable_data_aug, use_image
             def tf_image_map(image):
                 seed_table = rng.make_seeds(6)[0]
                 image = tf.image.stateless_random_brightness(image, max_delta=0.1, seed=seed_table[0:2])
-                image = tf.image.stateless_random_contrast(image, lower=0.9, upper=1.0, seed=seed_table[2:4])
-                image = tf.image.stateless_random_saturation(image, lower=0.9, upper=1.0, seed=seed_table[4:6])
+                image = tf.image.stateless_random_contrast(image, lower=0.9, upper=1.1, seed=seed_table[2:4])
+                image = tf.image.stateless_random_saturation(image, lower=0.9, upper=1.1, seed=seed_table[4:6])
                 return image
 
-            train_ds = train_ds.shuffle(128, seed=seed)
-            validation_ds = validation_ds.shuffle(128, seed=seed)
+            train_ds = train_ds.shuffle(128, seed=seed).repeat()
+            validation_ds = validation_ds.shuffle(128, seed=seed).repeat()
 
-            train_ds = train_ds.map(lambda image, mask: (tf_image_map(image), mask), num_parallel_calls=tf.data.AUTOTUNE)
+            train_ds = train_ds.map(lambda image, mask: (tf_image_map(image), mask), num_parallel_calls=tf.data.AUTOTUNE, deterministic=None)
 
             train_ds = train_ds.batch(batch_size)
             validation_ds = validation_ds.batch(batch_size)
 
-            train_ds = train_ds.map(lambda image, mask: (image_model_map(image, training=True), mask_model_map(mask, training=True)), num_parallel_calls=1)
+            train_ds = train_ds.map(lambda image, mask: (image_model_map(image, training=True), mask_model_map(mask, training=True)), num_parallel_calls=tf.data.AUTOTUNE, deterministic=None)
 
             train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
             validation_ds = validation_ds.prefetch(tf.data.AUTOTUNE)
@@ -97,8 +94,8 @@ def train_preprocessing(train_X, train_Y, batch_size, enable_data_aug, use_image
         train_ds = tf.data.Dataset.from_tensor_slices((train_X, train_Y)).take(validation_split_count)
         validation_ds = tf.data.Dataset.from_tensor_slices((train_X, train_Y)).skip(validation_split_count)
 
-        train_ds = train_ds.shuffle(128, seed=seed).batch(batch_size).prefetch(tf.data.AUTOTUNE)
-        validation_ds = validation_ds.shuffle(128, seed=seed).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+        train_ds = train_ds.shuffle(128, seed=seed).repeat().batch(batch_size).prefetch(tf.data.AUTOTUNE)
+        validation_ds = validation_ds.shuffle(128, seed=seed).repeat().batch(batch_size).prefetch(tf.data.AUTOTUNE)
 
         return train_ds, validation_ds
 
