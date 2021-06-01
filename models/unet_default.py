378dcf755857327f4dd287333e704dbd9d5288ce
Yi-Chen <yichen@powcyc.com>
2021-06-01 08:33:42 +0800

    update unet_default

--- models/unet_default.py
+++ models/unet_default.py
@@ -21,47 +21,57 @@ def unet_default(model_name, input_dim):
 
     # Setup
     filter_factor = 64
-    kernel_size = (3, 3)
-    strides = (2, 2)
+    conv2D_kernel_size = (3, 3)
+    resample_size = (2, 2)
+    resample_strides = (2, 2)
     padding = 'same'
-    kernel_initializer = 'he_normal'
+    kernel_initializer = 'he_uniform'
     activation = 'relu'
 
     # Encoder Architecture
     # Block 1
-    en_b1 = Conv2D_block(input=Input_img, element_num=2, filters=(filter_factor*math.pow(2, 0)), kernel_size=kernel_size, padding=padding, kernel_initializer=kernel_initializer, activation=activation, block_name='encoder_b1')
-    en_b1_pooling = MaxPooling2D((2, 2), strides=strides, padding=padding, name='block1_pool')(en_b1)
+    en_b1 = Conv2D_block(input=Input_img, element_num=3, filters=(filter_factor*math.pow(2, 0)), kernel_size=conv2D_kernel_size, padding=padding, kernel_initializer=kernel_initializer, activation=activation, block_name='encoder_b1')
+    en_b1_pooling = MaxPooling2D(resample_size, strides=resample_strides, padding=padding, name='block1_pool')(en_b1)
 
     # Block 2
-    en_b2 = Conv2D_block(input=en_b1_pooling, element_num=2, filters=(filter_factor*math.pow(2, 1)), kernel_size=kernel_size, padding=padding, kernel_initializer=kernel_initializer, activation=activation, block_name='encoder_b2')
-    en_b2_pooling = MaxPooling2D((2, 2), strides=strides, padding=padding, name='block2_pool')(en_b2)
+    en_b2 = Conv2D_block(input=en_b1_pooling, element_num=3, filters=(filter_factor*math.pow(2, 1)), kernel_size=conv2D_kernel_size, padding=padding, kernel_initializer=kernel_initializer, activation=activation, block_name='encoder_b2')
+    en_b2_pooling = MaxPooling2D(resample_size, strides=resample_strides, padding=padding, name='block2_pool')(en_b2)
 
     # Block 3
-    en_b3 = Conv2D_block(input=en_b2_pooling, element_num=3, filters=(filter_factor*math.pow(2, 2)), kernel_size=kernel_size, padding=padding, kernel_initializer=kernel_initializer, activation=activation, block_name='encoder_b3')
-    en_b3_pooling = MaxPooling2D((2, 2), strides=strides, padding=padding, name='block3_pool')(en_b3)
+    en_b3 = Conv2D_block(input=en_b2_pooling, element_num=3, filters=(filter_factor*math.pow(2, 2)), kernel_size=conv2D_kernel_size, padding=padding, kernel_initializer=kernel_initializer, activation=activation, block_name='encoder_b3')
+    en_b3_pooling = MaxPooling2D(resample_size, strides=resample_strides, padding=padding, name='block3_pool')(en_b3)
+
+    # Block 4
+    # en_b4 = Conv2D_block(input=en_b3_pooling, element_num=2, filters=(filter_factor*math.pow(2, 3)), kernel_size=conv2D_kernel_size, padding=padding, kernel_initializer=kernel_initializer, activation=activation, block_name='encoder_b4')
+    # en_b4_pooling = MaxPooling2D(resample_size, strides=resample_strides, padding=padding, name='block4_pool')(en_b4)
 
     encoder = en_b3_pooling
 
     # Decoder Architecture
     # Block 1
-    de_b1 = Conv2DTranspose(filters=(filter_factor*math.pow(2, 2)), kernel_size=kernel_size, strides=strides, padding=padding, kernel_initializer=kernel_initializer, name='decoder_b1_convT')(encoder)
+    de_b1 = Conv2DTranspose(filters=(filter_factor*math.pow(2, 2)), kernel_size=resample_size, strides=resample_strides, padding=padding, name='decoder_b1_convT')(encoder)
     de_b1 = Concatenate(axis=3)([en_b3, de_b1])
-    de_b1 = Conv2D_block(input=de_b1, element_num=3, filters=(filter_factor*math.pow(2, 2)), kernel_size=kernel_size, padding=padding, kernel_initializer=kernel_initializer, activation=activation, block_name='decoder_b1')
+    de_b1 = Conv2D_block(input=de_b1, element_num=3, filters=(filter_factor*math.pow(2, 2)), kernel_size=conv2D_kernel_size, padding=padding, kernel_initializer=kernel_initializer, activation=activation, block_name='decoder_b1')
 
     # Block 2
-    de_b2 = Conv2DTranspose(filters=(filter_factor*math.pow(2, 1)), kernel_size=kernel_size, strides=strides, padding=padding, kernel_initializer=kernel_initializer, name='decoder_b2_convT')(de_b1)
+    de_b2 = Conv2DTranspose(filters=(filter_factor*math.pow(2, 1)), kernel_size=resample_size, strides=resample_strides, padding=padding, name='decoder_b2_convT')(de_b1)
     de_b2 = Concatenate(axis=3)([en_b2, de_b2])
-    de_b2 = Conv2D_block(input=de_b2, element_num=2, filters=(filter_factor*math.pow(2, 1)), kernel_size=kernel_size, padding=padding, kernel_initializer=kernel_initializer, activation=activation, block_name='decoder_b2')
+    de_b2 = Conv2D_block(input=de_b2, element_num=3, filters=(filter_factor*math.pow(2, 1)), kernel_size=conv2D_kernel_size, padding=padding, kernel_initializer=kernel_initializer, activation=activation, block_name='decoder_b2')
 
     # Block 3
-    de_b3 = Conv2DTranspose(filters=(filter_factor*math.pow(2, 0)), kernel_size=kernel_size, strides=strides, padding=padding, kernel_initializer=kernel_initializer, name='decoder_b3_convT')(de_b2)
+    de_b3 = Conv2DTranspose(filters=(filter_factor*math.pow(2, 0)), kernel_size=resample_size, strides=resample_strides, padding=padding, name='decoder_b3_convT')(de_b2)
     de_b3 = Concatenate(axis=3)([en_b1, de_b3])
-    de_b3 = Conv2D_block(input=de_b3, element_num=2, filters=(filter_factor*math.pow(2, 0)), kernel_size=kernel_size, padding=padding, kernel_initializer=kernel_initializer, activation=activation, block_name='decoder_b3')
+    de_b3 = Conv2D_block(input=de_b3, element_num=3, filters=(filter_factor*math.pow(2, 0)), kernel_size=conv2D_kernel_size, padding=padding, kernel_initializer=kernel_initializer, activation=activation, block_name='decoder_b3')
+
+    # Block 4
+    # de_b4 = Conv2DTranspose(filters=(filter_factor*math.pow(2, 0)), kernel_size=resample_size, strides=resample_strides, padding=padding, name='decoder_b4_convT')(de_b3)
+    # de_b4 = Concatenate(axis=3)([en_b1, de_b4])
+    # de_b4 = Conv2D_block(input=de_b4, element_num=2, filters=(filter_factor*math.pow(2, 0)), kernel_size=conv2D_kernel_size, padding=padding, kernel_initializer=kernel_initializer, activation=activation, block_name='decoder_b4')
 
     decoder = de_b3
 
     # Output
-    output = Conv2D(filters=1, kernel_size=kernel_size, padding=padding, kernel_initializer='glorot_normal', name='output_conv')(decoder)
+    output = Conv2D(filters=1, kernel_size=(1, 1), padding=padding, kernel_initializer='glorot_uniform', name='output_conv')(decoder)
 
     model = Model(inputs=Input_img, outputs=output, name=model_name)
     return model
